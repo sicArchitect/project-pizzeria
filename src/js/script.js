2cec5311ff956c1ae521ab612e58c74b073c5aca
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-03-10 19:46:07 +0200

    Complete calculation of price

--- src/js/script.js
+++ src/js/script.js
@@ -65,6 +65,7 @@
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
       thisProduct.processOrder();
+      thisProduct.initAmountWidget();
 
       //console.log('new Product: ', thisProduct);
     }
@@ -112,8 +113,14 @@
         select.menuProduct.priceElem
       );
 
+      // looking for images of pizza
       thisProduct.imageWrapper = thisProduct.element.querySelector(
         select.menuProduct.imageWrapper
+      );
+
+      // looking for addition/subtraction of amount products
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(
+        select.menuProduct.amountWidget
       );
     }
 
@@ -165,7 +172,6 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      //console.log('formData: ', formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -174,29 +180,21 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxex' ...}
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
 
         //for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = {label: 'Olives', price: 2, default: true}
           const option = param.options[optionId];
-          //console.log(optionId, option);
 
           const optionChecking =
             formData[paramId] && formData[paramId].includes(optionId);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (optionChecking) {
-            //check if the option is not default
-            if (!price.default == true) {
-              // add option price to price variable
-              price += option.price;
-
-              //check if the option is default
-            } else if (option.default == true) {
-              // reduce price variable
-              price -= option.default;
-            }
+          if (optionChecking && !option.default == true) {
+            price += option.price;
+          } else if (!optionChecking && option.default == true) {
+            // reduce price variable
+            price -= option.price;
           }
 
           // find image
@@ -216,6 +214,21 @@
 
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
+    }
+
+    initAmountWidget() {
+      const thisProduct = this;
+
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+    }
+  }
+
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+
+      console.log('AmountWidget', thisWidget);
+      console.log('constructor arguments: ', element);
     }
   }
 
